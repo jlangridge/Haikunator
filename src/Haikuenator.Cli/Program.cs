73c3227bedfab77b8d93969f0ab6e8e90e8245d2
jlangridge <james.langridge.dev@gmail.com>
2012-11-26 20:21:03 +0000

    Now reads from a twitter timeline..

--- src/Haikuenator.Cli/Program.cs
+++ src/Haikuenator.Cli/Program.cs
@@ -1,39 +1,60 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Xml.Linq;
 
 namespace Haikuenator.Cli
 {
     public class Program
     {
-        public static void Main()
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Haiku.......");
-            using(var reader = File.OpenText("HaikuTest.txt"))
+            if(args.Length != 1)
             {
-                while(!reader.EndOfStream)
-                {
-                    var hk = new Haiku();
-                    
-                    try
-                    {
-                        
-                        hk.ReadFrom(reader);
-                        Console.WriteLine(hk);
-                        Console.WriteLine("======================");
-                        
-                        Console.Read();
-                    }
-                    catch (ArgumentException)
-                    {
-                        //Um, yeah... this is quick & dirty test code
-                    }
+                ShowUsage();
+                Environment.Exit(1);
+            }
 
-                    var tokenizer = new WordTokenizer(reader);
-                    tokenizer.NextToken();
+            var statuses = LoadStatuses(args[0]);
+            foreach (var status in statuses)
+            {
+                var hk = new Haiku();
+                try
+                {
+                    hk.ReadFrom(new StringReader(status));    
+                    Console.WriteLine(hk);
+                    Console.WriteLine("===============================");
                 }
-                
+                catch(ArgumentException)
+                {
+                    
+                }
+
             }
             Console.Read();
+        }
+
+        private static void ShowUsage()
+        {
+            Console.WriteLine("Haikuenates twitter feeds (kind of)");
+            Console.WriteLine("Usage: Haikuenate [screen name]");
+            Console.WriteLine("That is all..");
+        }
+
+        public static IEnumerable<string> LoadStatuses(string username)
+        {
+            var urlString = string.Format(
+                "http://api.twitter.com/1/statuses/user_timeline.xml?screen_name={0}", username);
+            var client = new WebClient();
+
+            var xmlStatuses = client.DownloadString(new Uri(urlString));
+
+            var xmlTweets = XElement.Parse(xmlStatuses); 
+
+            return from tweet in xmlTweets.Descendants("status")
+                          select (string)tweet.Element("text");
         }
     }
 }
