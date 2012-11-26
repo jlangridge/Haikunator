23699f5e7538e70dbe0a1efa8bafce44c4c4cab1
jlangridge <james.langridge.dev@gmail.com>
2012-11-26 14:58:19 +0000

    Very basic word tokenization

---
+++ src/Haikuenator/WordTokenizer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Haikuenator
+{
+    /// <summary>
+    ///   Imperfect (good-enough?) implementation of a lexical tokenizer. Not suitable for anything other than a bit of fun..
+    /// </summary>
+    public class WordTokenizer
+    {
+        private readonly string Source;
+
+        public WordTokenizer(string source)
+        {
+            Source = source;
+        }
+
+        public IEnumerable<string> ParseTokens()
+        {
+            return Source.Split(' ');
+        }
+    }
+}
---
+++ tests/Haikuenator.Tests/WordTokenizerTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Haikuenator.Tests
+{
+    [TestFixture]
+    public class WordTokenizerTests
+    {
+        [Test]
+        public void TokenizerShouldTokenizeWordsBySpaces()
+        {
+            var tokenizer = new WordTokenizer("This is a test");
+            var results = tokenizer.ParseTokens().ToArray();
+            Assert.AreEqual(new[]{"This", "is", "a", "test"}, results);
+        }
+    }
+}
