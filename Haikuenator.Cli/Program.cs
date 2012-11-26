e7e7423d2802105a70ecd34239c0cfbe93490695
jlangridge <james.langridge.dev@gmail.com>
2012-11-26 19:30:53 +0000

    Reads from a file now..

--- Haikuenator.Cli/Program.cs
+++ Haikuenator.Cli/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 
 namespace Haikuenator.Cli
 {
@@ -9,7 +7,33 @@ namespace Haikuenator.Cli
     {
         public static void Main()
         {
-            
+            Console.WriteLine("Haiku.......");
+            using(var reader = File.OpenText("HaikuTest.txt"))
+            {
+                while(!reader.EndOfStream)
+                {
+                    var hk = new Haiku();
+                    
+                    try
+                    {
+                        
+                        hk.ReadFrom(reader);
+                        Console.WriteLine(hk);
+                        Console.WriteLine("======================");
+                        
+                        Console.Read();
+                    }
+                    catch (ArgumentException)
+                    {
+                        //Um, yeah... this is quick & dirty test code
+                    }
+
+                    var tokenizer = new WordTokenizer(reader);
+                    tokenizer.NextToken();
+                }
+                
+            }
+            Console.Read();
         }
     }
 }
