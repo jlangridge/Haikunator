f1def57ab7075a70333203c4e79fbd186c5a3a9a
jlangridge <james.langridge.dev@gmail.com>
2012-11-26 15:52:08 +0000

    Basic syllable analysis

---
+++ src/Haikuenator/SyllableAnalyzer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Haikuenator
+{
+    public class SyllableAnalyzer
+    {
+        private readonly string Word;
+
+        public SyllableAnalyzer(string word)
+        {
+            Word = word.ToLower();
+        }
+
+        public IEnumerable<string> ParseVowelGroups()
+        {
+            var vowels = new[] {'a', 'e', 'i', 'o', 'u', 'y'};
+
+            var isFirst = true;
+            var group = string.Empty;
+
+            foreach (var letter in Word)
+            {
+                if (!vowels.Contains(letter))
+                {
+                    group += letter;
+                    continue;
+                }
+                if(!isFirst)
+                {
+                    yield return @group;
+                    @group = string.Empty;
+                }
+                group += letter;
+                isFirst = false;
+            }
+
+            if(!string.IsNullOrEmpty(group))
+            {
+                yield return group;
+            }
+        }
+
+        public int GetCount()
+        {
+            return ParseVowelGroups().Count();
+        }
+    }
+}
---
+++ tests/Haikuenator.Tests/SyllableAnalyzerTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Haikuenator.Tests
+{
+    [TestFixture]
+    public class SyllableAnalyzerTests
+    {
+        [Test]
+        public void SyllableAnalyzerShouldSplitByVowel()
+        {
+            var analyzer = new SyllableAnalyzer("testing");
+            var groups = analyzer.ParseVowelGroups().ToArray();
+            Assert.AreEqual(new[]{"test", "ing"}, groups);
+        }
+
+        [Test]
+        public void SyllableAnalyzerShouldCountBasicWordsCorrectly()
+        {
+            var analyzer = new SyllableAnalyzer("fundamental");
+            Assert.AreEqual(4, analyzer.GetCount());
+        }
+    }
+}
